9ad422df174e561070d9c1c4e7b5aabde1db9bb2
Abogeerick <abogeerick@gmail.com>
2023-06-12 11:17:38 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+// index.js
+
+// Define the variables
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+// Export the variables
+module.exports = {
+  companyName,
+  mostProfitableNeighborhood,
+  companyCeo
+};
